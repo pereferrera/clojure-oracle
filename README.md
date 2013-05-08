da53afcdf7e4bd13ef99ce3edf9d716d5c1603f7
pere <pere@datasalt.com>
2013-05-08 19:28:52 +0200

    Improved version of the super-oracle.

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 A simple Clojure project that answers quick questions like "How does Lady Gaga in the WC look like?".
 
-Use maven for compiling it. Load oracle.clj in the REPL, and ask a question. Actually, you can only ask one kind of question as of today: "How does ... look like?". But it can be a lot of fun anyway!
+Use maven for compiling it. Load oracle.clj in the REPL, and ask a question. Questions you can ask:
 
 	(ask "How does Lady Gaga in the WC look like?")
 
-Note: This will pop-up the answer using "google-chrome" so you should have it in the path.
+This will pop-up an image in google.
+
+	(ask "Where is the Berlin wall?")
+	
+This will pop up a maps page in google maps.
+
+	(ask "Which is that video about a grumpy cat?")
+	
+This will pop up a video in youtube.
+Note: The answers are pop-up using "google-chrome" so you should have it in the path.
--- src/main/clojure/oracle.clj
+++ src/main/clojure/oracle.clj
@@ -1,5 +1,6 @@
 (import 'java.lang.Runtime)
-
+(require 'clojure.string)
+ 
 (ns oracle)
 
 (defn fetch [url]
@@ -8,13 +9,32 @@
 (defn encode [partial_url]
   (java.net.URLEncoder/encode partial_url))
 
-(defn images_of [name]  
+(defn images-of [name]  
   (map (fn [vec] (nth vec 1)) 
        (re-seq #"imgurl=([^\&]*)\&" 
                (fetch (str "http://www.google.com/search?tbm=isch&q=" (encode name))))))
 
+(defn videos-of [name]
+  (map (fn [vec] (str "http://www.youtube.com/watch?v=" (nth vec 1))) 
+       (re-seq #"watch\?v=([^\"]*)\"" 
+               (fetch (str "http://www.youtube.com/results?q=" (encode name))))))
+
+(defn where [place]
+  (str "https://maps.google.com/maps?geocode=&q=" (encode place)))
+
 (defn show [url]
   (. (Runtime/getRuntime) exec (str "google-chrome " url)))
 
+(defn ask-where [question]
+  (where (nth  2)))
+
 (defn ask [question]
-  (show (first (images_of (nth (re-matches #"(\QHow does \E)([^\?]*)(\Q look like\E\?)" question) 2)))))
+  (let [l-question (clojure.string/lower-case question)
+        video-matches (re-matches #"(\Qwhich\E|\Qwhat\E)(\Q is that\E|\Q is this\E)(\Q video about \E)([^\?]*)\?" l-question)
+        image-matches (re-matches #"(\Qhow \E)(\Qdoes\E|\Qdo\E) ([^\?]*)(\Q look like\E\?)" l-question)
+        place-matches (re-matches #"(\Qwhere is \E)([^\?]*)(\?)" l-question)] 
+    (cond
+      video-matches (show (first (videos-of (nth video-matches 4))))
+      place-matches (show (where (nth place-matches 2)))
+      image-matches (show (first (images-of (nth image-matches 3))))
+      :else "Question not understood")))
